a259826ea87a9dd85c8e34d4d7460ee82fcd6560
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-04 08:45:50 +0600

    Restart

--- script.js
+++
@@ -1,9 +0,0 @@
-const link = "http://api.weatherstack.com/current?access_key=a923fb5a3f626866873abb3056805467";
-
-const fetchData = async () => {
-    const result = await fetch('${link}&query=London');
-    const data = await result.json();
-
-    console.log(data);
-};
-fetchData();
