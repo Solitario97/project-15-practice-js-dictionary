ad44aefab14a0cb627d5187c10b85d49b7edafe2
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-04 10:16:18 +0600

    i don`t understand, but it`s realy interesting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,26 @@
 // TODO: добавить функционал для воспроизведения звука
 // TODO: вставить в контейнер с результатами
 
+
+let state = {
+  word: '',
+};
+
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+const input = document.getElementById('word-input');
+const form = document.querySelector('.form');
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const response = await fetch('${url}${state.word}');
+  const data = await response.json();
+};
+
+const handleKeyup = (e) => {
+  const value = e.target.value;
+  state.word = value;
+};
+// events
+
+input.addEventListener('keyup', handleKeyup);
+form.addEventListener('sabmit', handleSubmit);
