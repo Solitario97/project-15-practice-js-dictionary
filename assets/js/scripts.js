f9a578db6d4ef479f947ab0c9b2c6a0ad57ea3e8
Solitario97 <a.nurmagambetov97@gmail.com>
2023-02-05 08:21:19 +0600

    its so hard fo me

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,17 +11,35 @@ let state = {
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 const input = document.getElementById('word-input');
 const form = document.querySelector('.form');
+const containerWord = document.querySelector('.results-word');
+
+const insertWord = () => {
+  containerWord.innerText = state.word;
+};
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-  const response = await fetch('${url}${state.word}');
-  const data = await response.json();
+
+  if (!state.word.trim()) return;
+
+  try {
+    const response = await fetch(`${url}${state.word}`);
+    const data = await response.json();
+
+    if(response.ok && data.length){
+      insertWord();
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const handleKeyup = (e) => {
   const value = e.target.value;
   state.word = value;
 };
+
+
 // events
 
 input.addEventListener('keyup', handleKeyup);
